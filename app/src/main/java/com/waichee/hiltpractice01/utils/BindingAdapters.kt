93718a8c5cc84a030b579ee707f2c787e579336e
christina.chee <waichee723@gmail.com>
2020-08-15 23:04:45 +0800

    Enhance Movie List layout

--- app/src/main/java/com/waichee/hiltpractice01/utils/BindingAdapters.kt
+++ app/src/main/java/com/waichee/hiltpractice01/utils/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package com.waichee.hiltpractice01.utils
 
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
@@ -8,9 +9,25 @@ import timber.log.Timber
 
 @BindingAdapter("imageUrl")
 fun bindImage(imageView: ImageView, imageUrl: String?) {
-    Timber.i("bindImage")
     imageUrl?.let {
         Picasso.get().load("https://image.tmdb.org/t/p/w500$imageUrl").into(imageView)
+    }
+}
+
+@BindingAdapter("voteAverage")
+fun bindVoteAverage(textView: TextView, voteAverage: Double?) {
+    voteAverage?.let{
+        when (it) {
+            0.0 -> textView.text = "- -"
+            else -> textView.text = it.toString()
+        }
+    }
+}
+
+@BindingAdapter("voteAverageStar")
+fun bindVoteAverageStar(ratingBar: RatingBar, voteAverage: Double?) {
+    voteAverage?.let {
+        ratingBar.rating = (voteAverage/2).toFloat()
     }
 }
 
